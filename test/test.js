d5e2667bd41be88d42300492ab08594187518985
Antti Honkasalo <s1800074@student.businesscollege.fi>
2018-08-27 15:19:03 +0300

    update

--- test/test.js
+++ test/test.js
@@ -22,12 +22,14 @@ let assert = require('assert');
   describe('longestWord', function() {
     it('should return longest word when given following sentence', function() {
       assert.equal('beautiful', functions.longestWord('Sea is beautiful'));
+      assert.equal('mug', functions.longestWord('I am a mug'));
     });
     it('should return longest number if given numbers in text as parameters', function() {
       assert.equal('999922', functions.longestWord('999922 3333 44 5'));
     });
     it('should return "argument is not a string" when the sentence is not a string', function() {
       assert.equal('argument is not a string', functions.longestWord(5));
+      assert.equal('argument is not a string', functions.longestWord(true));
     });
   });
 
@@ -35,10 +37,13 @@ let assert = require('assert');
     it('should return number of vowels when given following string', function() {
       assert.equal(12, functions.vowelCount('banana örk in Alabama ÖÄå'));
     });
-    it('should return 0 when given following string', function() {
+    it('should return 0 when given a sentence with 0 vowels or string is empty', function() {
       assert.equal(0, functions.vowelCount('ghhgtwtw 1393878973 klwlkklwc'));
+      assert.equal(0, functions.vowelCount(''));
     });
     it('should return 0 when the sentence is not a string', function() {
       assert.equal(0, functions.vowelCount(true));
+      assert.equal(0, functions.vowelCount(124));
+      assert.equal(0, functions.vowelCount(null));
     });
   });
